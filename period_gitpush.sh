100e6e04568396e2b262ca6baaca0369e27f2a00
Ubuntu <ubuntu@ip-172-31-62-49.ec2.internal>
2016-12-18 07:32:18 +0000

    auto

--- period_gitpush.sh
+++ period_gitpush.sh
@@ -5,7 +5,8 @@ seconds=1800
 
 for i in {1..100}
 do
-    echo Time $i
+    echo Time $i -------------------------------------------------------------------------------------------
+    date
     git add .
     git commit -m "auto"
     git push
