9707dc177c79f791cbd56c3bd7f826704119f334
namiyao <disneydsy@gmail.com>
2017-01-01 06:06:22 +0000

    Manual

--- terminate_gitpush.sh
+++ terminate_gitpush.sh
@@ -4,7 +4,7 @@
 seconds=5
 status_terminate='"marked-for-termination"'
 
-for i in {1..10000000000000000000000}
+for i in {1..34560}
 do
     status=$(aws ec2 describe-spot-instance-requests --filters "Name=spot-instance-request-id,Values=sir-y7pg4e4k"| jq '.SpotInstanceRequests[0].Status.Code')
     if  [ $status = $status_terminate ]; then
