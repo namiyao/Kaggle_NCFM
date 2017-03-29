6399b255ab146bce7511bdc01dc7c05b4d399511
namiyao <disneydsy@gmail.com>
2017-03-29 15:12:35 +0000

    manual

--- period_gitpush.sh
+++ period_gitpush.sh
@@ -7,7 +7,7 @@ for i in {1..100}
 do
     echo Time $i ---------------------------------------
     date
-    aws s3 cp ~/Kaggle_NCFM/model_bbox s3://disneydsy/model_bbox --recursive
+    aws s3 cp ~/Kaggle_NCFM/model_bbox/resnet50_FT38_Hybrid_Rep s3://disneydsy/model_bbox/resnet50_FT38_Hybrid_Rep --recursive
     #aws s3 sync ~/Kaggle_NCFM s3://disneydsy/Kaggle_NCFM --exclude "data/*"
     git add .
     git commit -m "auto_period"
